ae55428f3771942b64f7a02d30fbf699b3ad5db2
Aram Peres <aram.peres@wavy.fm>
2021-04-07 00:26:25 -0400

    Remove unecessary copy in getting pawn moves; improve function doc

--- poirebot/src/game/pieces/pawn.rs
+++ poirebot/src/game/pieces/pawn.rs
@@ -2,8 +2,11 @@ use crate::bitboard::{BitBoard, EMPTY};
 use crate::game::pieces::{Color, FILE_A, FILE_H, RANK_2, RANK_3, RANK_4, RANK_5, RANK_6, RANK_7};
 use crate::game::Board;
 
-/// Generates a bitboard with the moves that can be performed by the pawn at the given origin.
-pub fn get_pawn_moves_and_attacks(board: Board, color: Color, origin: &BitBoard) -> BitBoard {
+/// Generates a bitboard with the moves that can be performed by the pawns in the given bitboard.
+///
+/// Note that multiple pawns can be passed in the bitboard; to get the moves for individual pawns,
+/// iterate over the pawns and call this function with the singular bitboard for each pawn.
+pub fn get_pawn_moves_and_attacks(board: &Board, color: Color, origin: &BitBoard) -> BitBoard {
     let mut result = EMPTY;
 
     let all_pieces = board.white.pieces | board.black.pieces;
@@ -73,42 +76,42 @@ mod tests {
 
         // a2 can do single-step or double-step
         assert_eq!(
-            super::get_pawn_moves_and_attacks(board, Color::White, &BitBoard::from_position("a2"))
+            super::get_pawn_moves_and_attacks(&board, Color::White, &BitBoard::from_position("a2"))
                 .collect::<Vec<Position>>(),
             vec![Position::from("a3"), Position::from("a4")]
         );
 
         // b2 can do single-step only
         assert_eq!(
-            super::get_pawn_moves_and_attacks(board, Color::White, &BitBoard::from_position("b2"))
+            super::get_pawn_moves_and_attacks(&board, Color::White, &BitBoard::from_position("b2"))
                 .collect::<Vec<Position>>(),
             vec![Position::from("b3")]
         );
 
         // d2 cannot move
         assert_eq!(
-            super::get_pawn_moves_and_attacks(board, Color::White, &BitBoard::from_position("d2"))
+            super::get_pawn_moves_and_attacks(&board, Color::White, &BitBoard::from_position("d2"))
                 .collect::<Vec<Position>>(),
             vec![]
         );
 
         // d5 can do en-passant or single-step
         assert_eq!(
-            super::get_pawn_moves_and_attacks(board, Color::White, &BitBoard::from_position("d5"))
+            super::get_pawn_moves_and_attacks(&board, Color::White, &BitBoard::from_position("d5"))
                 .collect::<Vec<Position>>(),
             vec![Position::from("c6"), Position::from("d6")]
         );
 
         // e3 can do single-step
         assert_eq!(
-            super::get_pawn_moves_and_attacks(board, Color::White, &BitBoard::from_position("e3"))
+            super::get_pawn_moves_and_attacks(&board, Color::White, &BitBoard::from_position("e3"))
                 .collect::<Vec<Position>>(),
             vec![Position::from("e4")]
         );
 
         // f4 can capture left, right, or single-step
         assert_eq!(
-            super::get_pawn_moves_and_attacks(board, Color::White, &BitBoard::from_position("f4"))
+            super::get_pawn_moves_and_attacks(&board, Color::White, &BitBoard::from_position("f4"))
                 .collect::<Vec<Position>>(),
             vec![
                 Position::from("e5"),
@@ -119,14 +122,14 @@ mod tests {
 
         // g3 can capture right, or single-step
         assert_eq!(
-            super::get_pawn_moves_and_attacks(board, Color::White, &BitBoard::from_position("g3"))
+            super::get_pawn_moves_and_attacks(&board, Color::White, &BitBoard::from_position("g3"))
                 .collect::<Vec<Position>>(),
             vec![Position::from("g4"), Position::from("h4")]
         );
 
         // test all pawn moves and attacks at once
         assert_eq!(
-            super::get_pawn_moves_and_attacks(board, Color::White, &board.white.pawns)
+            super::get_pawn_moves_and_attacks(&board, Color::White, &board.white.pawns)
                 .collect::<Vec<Position>>(),
             vec![
                 Position::from("a3"),
@@ -152,35 +155,35 @@ mod tests {
 
         // h8 can do single-step or double-step
         assert_eq!(
-            super::get_pawn_moves_and_attacks(board, Color::Black, &BitBoard::from_position("h7"))
+            super::get_pawn_moves_and_attacks(&board, Color::Black, &BitBoard::from_position("h7"))
                 .collect::<Vec<Position>>(),
             vec![Position::from("h5"), Position::from("h6")]
         );
 
         // h3 cannot move
         assert_eq!(
-            super::get_pawn_moves_and_attacks(board, Color::Black, &BitBoard::from_position("h3"))
+            super::get_pawn_moves_and_attacks(&board, Color::Black, &BitBoard::from_position("h3"))
                 .collect::<Vec<Position>>(),
             vec![]
         );
 
         // g4 can only do en-passant
         assert_eq!(
-            super::get_pawn_moves_and_attacks(board, Color::Black, &BitBoard::from_position("g4"))
+            super::get_pawn_moves_and_attacks(&board, Color::Black, &BitBoard::from_position("g4"))
                 .collect::<Vec<Position>>(),
             vec![Position::from("f3")]
         );
 
         // f7 cannot move
         assert_eq!(
-            super::get_pawn_moves_and_attacks(board, Color::Black, &BitBoard::from_position("f7"))
+            super::get_pawn_moves_and_attacks(&board, Color::Black, &BitBoard::from_position("f7"))
                 .collect::<Vec<Position>>(),
             vec![]
         );
 
         // c3 can capture left, right, or single-step
         assert_eq!(
-            super::get_pawn_moves_and_attacks(board, Color::Black, &BitBoard::from_position("c3"))
+            super::get_pawn_moves_and_attacks(&board, Color::Black, &BitBoard::from_position("c3"))
                 .collect::<Vec<Position>>(),
             vec![
                 Position::from("b2"),
@@ -191,21 +194,21 @@ mod tests {
 
         // b3 can capture left only
         assert_eq!(
-            super::get_pawn_moves_and_attacks(board, Color::Black, &BitBoard::from_position("b3"))
+            super::get_pawn_moves_and_attacks(&board, Color::Black, &BitBoard::from_position("b3"))
                 .collect::<Vec<Position>>(),
             vec![Position::from("a2")]
         );
 
         // a3 can capture right only
         assert_eq!(
-            super::get_pawn_moves_and_attacks(board, Color::Black, &BitBoard::from_position("a3"))
+            super::get_pawn_moves_and_attacks(&board, Color::Black, &BitBoard::from_position("a3"))
                 .collect::<Vec<Position>>(),
             vec![Position::from("b2")]
         );
 
         // test all pawn moves and attacks at once
         assert_eq!(
-            super::get_pawn_moves_and_attacks(board, Color::Black, &board.black.pawns)
+            super::get_pawn_moves_and_attacks(&board, Color::Black, &board.black.pawns)
                 .collect::<Vec<Position>>(),
             vec![
                 Position::from("a2"),
--- poirebot/src/genius.rs
+++ poirebot/src/genius.rs
@@ -107,7 +107,7 @@ fn list_potential_moves(board: Board, color: Color) -> BTreeSet<BrainMove> {
             "pawn" => side
                 .pawns
                 .flat_map(|pawn| {
-                    pieces::pawn::get_pawn_moves_and_attacks(board, color, &BitBoard::from(pawn))
+                    pieces::pawn::get_pawn_moves_and_attacks(&board, color, &BitBoard::from(pawn))
                         .map(move |dest| Move::from((pawn, dest)).with_promotion(promote(dest)))
                 })
                 .collect(),
